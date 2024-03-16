1cf7f25c657e620ccdf2e57f56b2ce49be034b0b
Tracy Karanja <chirutracy@gmail.com>
2024-03-16 23:01:23 +0300

    New file

--- index.js
+++ index.js
@@ -1,34 +1,29 @@
 // Write your solution in this file!
 const employee = {
-    name : 'sam',
-    streetAddress : '11 Broadway'
+    name: 'Sam',
+    streetAddress: '11 Broadway'
 };
 
-function updateEmployeeWithKeyAndValue(employee, key, value){
-    const newEmployee = {...employee};
-    newEmployee[key] = value;  // Use newEmployee to update the clone, not the original object
-    return newEmployee;
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return { ...employee, [key]: value };
 }
 
-function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
     employee[key] = value;
     return employee;
 }
 
-function deleteFromEmployeeByKey(employee, key){
-    let newEmployee = {...employee};
-    if (newEmployee.hasOwnProperty(key)) { // Check if the key exists in the object
-        delete newEmployee[key];
-    }
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = { ...employee };
+    delete newEmployee[key];
     return newEmployee;
 }
 
-function destructivelyDeleteFromEmployeeByKey(employee, key){
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
     delete employee[key];
     return employee;
 }
 
-// Test your functions
 updateEmployeeWithKeyAndValue(employee, 'streetAddress', '11 Broadway');
 destructivelyUpdateEmployeeWithKeyAndValue(employee, 'streetAddress', '12 Broadway');
 deleteFromEmployeeByKey(employee, 'name');
